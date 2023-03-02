afdc657eaf435c019208168811e3e96c15ad332a
TheHighnote <ryanhighnote7@gmail.com>
2023-03-02 10:36:10 -0600

    added name and location to users

--- db/index.js
+++ db/index.js
@@ -6,22 +6,30 @@ const client = new Client("postgres://localhost:5432/juicebox");
 
 async function getAllUsers() {
   const { rows } = await client.query(
-    `SELECT id, username
-        FROM users;
+    `SELECT id, 
+    username,
+    name,
+    location,
+    active
+     FROM users;
         `
   );
   return rows;
 }
 
-async function createUser({ username, password }) {
+async function createUser({ 
+  username,
+  password,
+  name,
+  location 
+}) {
   try {
     const result = await client.query(
-      `INSERT INTO users(username, password)
-        VALUES ($1, $2)
+      `INSERT INTO users(username, password, name, location)
+        VALUES ($1, $2, $3, $4)
         ON CONFLICT (username) DO NOTHING 
       RETURNING *;
-      `, [username, password]
-    );
+      `, [username, password, name, location]);
 
     return result;
   } catch (error) {
