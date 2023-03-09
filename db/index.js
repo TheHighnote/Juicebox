3cfd44f0ade935a1108554c7c64f20623e429e69
TheHighnote <ryanhighnote7@gmail.com>
2023-03-09 08:56:51 -0600

    modified index.js

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@
 const { Client } = require("pg"); // imports the pg module
 const connectionString = process.env.DATABASE_URL || "postgres://localhost:5432/juicebox"
 // supply the db name and location of the database
-const client = new Client(connectionString);
+const client = new Client({connectionString, ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,});
 
 async function getAllUsers() {
   const { rows } = await client.query(
